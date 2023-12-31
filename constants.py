f0963c8e5eeb0e059dc4365294dc30f424ba6bc5
Cesaryuan <Cesaryuan@qq.com>
2023-12-31 15:42:28 +0800

    feat: change relative path to absolute

--- constants.py
+++ constants.py
@@ -1,8 +1,11 @@
+from os import path
+
+
 class Constants:
-    PATH_COUNTRY = './data/country.json'
-    PATH_REGION = './data/region.json'
-    PATH_CITY = './data/city.json'
-    PATH_DB = './data/IP2LOCATION-LITE-DB5.IPV6.BIN'
+    PATH_COUNTRY = path.join(path.dirname(__file__), 'data', 'country.json')
+    PATH_REGION = path.join(path.dirname(__file__), 'data', 'region.json')
+    PATH_CITY = path.join(path.dirname(__file__), 'data', 'city.json')
+    PATH_DB = path.join(path.dirname(__file__), 'data', 'IP2LOCATION-LITE-DB5.IPV6.BIN')
     INDEX_COUNTRY = 1
     INDEX_REGION = 2
     INDEX_CITY = 3
